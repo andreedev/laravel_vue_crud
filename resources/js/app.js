618bd4cd413ce49f034034e6b778ccc5ab514118
andree <andreemath27@gmail.com>
2020-10-21 10:11:32 -0500

    vuex instalado

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+window.Vuex = require('vuex')
+
 axios.defaults.baseURL = 'http://localhost/projects/14.laravel_vue_crud/public';
+
+Vue.use(Vuex);
 
 Vue.component('my-thoughts-component', require('./components/MyThoughtsComponent.vue').default);
 Vue.component('thought-component', require('./components/ThoughtComponent.vue').default);
